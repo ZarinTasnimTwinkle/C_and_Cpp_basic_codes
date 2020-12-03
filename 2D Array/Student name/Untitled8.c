060787ddb0eaced41ede7db57393f9ae1d8adee7
Zarin Tasnim <75076461+ZarinTasnimTwinkle@users.noreply.github.com>
2020-12-04 00:41:17 +0600

    Add files via upload

---
+++ 2D Array/Student name/Untitled8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char student[200][40], temp[40];
+    int i,j,n;
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++)
+        scanf("%s",student[i]);
+
+        for(i=0;i<n;i++)
+            printf("\nstudent name-%d: %s",i+1,student[i]);
+
+    for(i=1;i<n;i++){
+        for(j=0;j<n-i;j++){
+            if(strcmp(student[j],student[j+1])>0){
+                strcpy(temp,student[j]);
+                strcpy(student[j],student[j+1]);
+                strcpy(student[j+1],temp);
+            }
+        }
+    }
+    printf("\n");
+
+    for(i=0;i<n;i++)
+        printf("\n%d.%s",i+1,student[i]);
+        printf("\n");
+
+    return 0;
+}
+
